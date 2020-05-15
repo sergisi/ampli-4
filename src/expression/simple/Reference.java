c69794dd27cc924a49ac6211b770c272e7634ef3
Horno <i.palacin.a@gmail.com>
2020-05-15 17:02:34 +0200

    Implemented cell and reference, updated gitignore

--- src/expression/simple/Reference.java
+++ src/expression/simple/Reference.java
@@ -1,4 +1,33 @@
 package expression.simple;
 
-public class Reference {
+import expression.Expression;
+import sheet.Cell;
+
+import java.util.Objects;
+
+public class Reference extends Expression {
+
+    Cell ref;
+
+    public Reference(Cell ref) {
+        this.ref = ref;
+    }
+
+    @Override
+    public MaybeValue evaluate() {
+        return ref.evaluate();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Reference reference = (Reference) o;
+        return Objects.equals(ref, reference.ref);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ref);
+    }
 }
--- src/sheet/Cell.java
+++ src/sheet/Cell.java
@@ -1,4 +1,42 @@
 package sheet;
 
-public class Cell {
+import expression.Expression;
+import expression.simple.MaybeValue;
+import expression.simple.NoValue;
+
+import java.util.Objects;
+
+public class Cell extends Expression {
+
+    Expression expression;
+
+    public Cell(){
+        this.expression = new NoValue();
+    }
+
+    public Cell(Expression expression){
+        this.expression = expression;
+    }
+
+    @Override
+    public MaybeValue evaluate() {
+        return expression.evaluate();
+    }
+
+    public void set(Expression exp){
+        this.expression = exp;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cell cell = (Cell) o;
+        return Objects.equals(expression, cell.expression);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(expression);
+    }
 }
